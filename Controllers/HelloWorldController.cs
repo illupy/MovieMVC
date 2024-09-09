e4febd27188f916b2bab86cb2574dd2b5255266a
illupy <tailuuduc2004@gmail.com>
2024-09-09 16:34:04 +0700

    Mvc Movie

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+	public class HelloWorldController : Controller
+	{
+		//public IActionResult Index()
+		//{
+		//	return View();
+		//}
+		//GET :/HelloWorld
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public string Welcome(string name, int ID = 1)
+		{
+			return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+		}
+	}
+}
